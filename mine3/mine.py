0fadb3ae3be81abaec89f005cb1953fe384e6f88
yanheven <yanheven@qq.com>
2015-11-14 09:37:13 +0800

    fix quantity

--- mine3/mine.py
+++ mine3/mine.py
@@ -1,8 +1,5 @@
 #_*_ coding: UTF-8 _*_
-__author__ = 'evan'
-
-import time
-
+#__author__ = 'evan'
 import logger
 import login
 import nomore_mine
@@ -14,20 +11,20 @@ def buy(session, code, price):
     balance = float(nomore_mine.BALANCE)
     if not balance:
         return False
-    quantity = int(balance / price * 100)
+    quantity = int(balance / price / 100)
     url = nomore_mine.TRANSACT_URL
-    BODY = nomore_mine.BUY_BODY
-    body = BODY
+    body = nomore_mine.BUY_BODY
     body['SECU_CODE'] = code
     body['QTY'] = quantity
     try:
         res = session.post(url, headers=nomore_mine.HEADERS,
-                             data=body)
+                           data=body)
     except Exception as e:
         LOG.error('market buy Fail %s' % e)
     else:
         LOG.warn('market buy: %d %s code: %s quantity: %d' %
                  (res.status_code, res.content, code, quantity))
+
 
 def get_history(session):
     url = nomore_mine.TRANSACT_URL
@@ -45,15 +42,7 @@ def get_balance(session):
     try:
         res = session.post(url, headers=nomore_mine.HEADERS, data=body)
         content = res.content
-        # content = content.decode('gbk', 'ignore')
-        # content = content.encode(encoding='gbk')
-        content += u''
-        print(res.status_code)
-        print(content)
-        content = content.split('\x06X\x06\t')
-        print(content)
-        content = content.split('\x06Z\x06Z\x06Z\x06\t6100\x06X\x06Z\x06P\x06X'
-                                '\x06Z\x06X\x06\x0f')[0]
+
     except Exception as e:
         LOG.error('get balance Fail :%s' % e)
         return False
@@ -62,6 +51,7 @@ def get_balance(session):
         balance = float(content)
         LOG.warn('get balance: %d balance: %f' % (res.status_code, balance))
         return balance
+
 
 def keep_awake(session):
     url = nomore_mine.TRANSACT_URL
