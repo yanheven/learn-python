b21a96ed8506ca1f3c3506371174f20af5769ba9
yanheven <yanheven@qq.com>
2017-03-02 22:26:21 +0800

    update email

--- email/send.py
+++ email/send.py
@@ -5,30 +5,106 @@ from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 import smtplib
+import datetime
+import csv
 
-from get_receivers import get_buyer
-from get_receivers import get_product_name
-from get_receivers import get_refund
+
+def read_csv(file_name):
+    reader = csv.reader(open(file_name), delimiter='\t')
+    return list(reader)[1:]
+
+
+def get_buyer(file_name):
+    buyers = read_csv(file_name)
+    buyer_dicts = []
+    yesterday = datetime.datetime.now() - datetime.timedelta(1)
+    yesterday = yesterday.strftime('%Y-%m-%d')
+    for i in buyers:
+        if yesterday == i[44][:10]:
+            buyer_dict = {}
+            buyer_dict['order_id'] = i[0]
+            buyer_dict['buyer_email'] = i[10]
+            buyer_dict['buyer_name'] = i[11].split(' ')[0]
+            buyer_dict['sku'] = i[13]
+            buyer_dicts.append(buyer_dict)
+    return buyer_dicts
+
+
+def get_product_name(file_name):
+    """{'sku':{'asin': 'xxxx', 'product_name': 'ssss'}}
+
+    :param file_name:
+    :return:
+    """
+    products = read_csv(file_name)
+    product_dicts = {}
+    for i in products:
+        product_dict = {}
+        product_dict['asin'] = i[2]
+        product_dict['product_name'] = i[3]
+        product_dict['market_domain'] = i[1]
+        product_dicts[i[0]] = product_dict
+    return product_dicts
+
+
+def get_refund(file_name):
+    refunds = read_csv(file_name)
+    refund_ids = []
+    for i in refunds:
+        refund_ids.append(i[1])
+    return refund_ids
+
 
 def _format_addr(s):
     name, addr = parseaddr(s)
-    return formataddr(( \
-        Header(name, 'utf-8').encode(), \
+    return formataddr((
+        Header(name, 'utf-8').encode(),
         addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
 
 # receivers = read_csv(raw_input('template name:'))
 order_file_name = 'order.txt'
 sku_file_name = 'sku.csv'
-refund_file_name = 'refund.txt'
+# refund_file_name = 'refund.txt'
 buys = get_buyer(order_file_name)
 skus = get_product_name(sku_file_name)
-refunds = get_refund(refund_file_name)
+# refunds = get_refund(refund_file_name)
 print(buys[0])
 
 from_addr = 'amazon@ohilltech.com'
 password = ''
 to_addr = 'yanheven@qq.com'
 smtp_server = 'smtp.exmail.qq.com'
+alarm_str = '\nWarm Tips: After you choose your favorite nature sound,' \
+            ' please remember to press the second button(♫ icon) on the' \
+            ' backing again to save the setting.\n\nUser Manual: You can download the user' \
+            ' manual on product page, go to "Product Information > Technical' \
+            ' Specification > User Manual [pdf ]", or via this link https://images-na.ssl-' \
+            'images-amazon.com/images/I/B18uhatYo2S.pdf\n'
+follow_str = '''
+Dear %s,
+
+Greetings! This is Elena from O'Hill customer service team.
+
+Please kindly be advised that we noticed that the package have delivered at you. May I know have you received it?
+
+This is a follow-up email to confirm whether you are completely happy with this purchase.
+If there was a problem with our product, please feel free to get back to us and we will help to get the problem solved ASAP.
+And if you are satisfied with our product and service, we would greatly appreciate it if you could share your user experience
+with your friends/ family or online.
+
+The following is the details for your Amazon order: %s
+
+%s
+%s
+To share your user experience, click: http://www.amazon.%s/dp/%s
+
+If you have any problems about our product, we would appreciate it if you would give us a chance to make things right for you. Thanks a lot.
+
+Wish you a great day ahead!
+
+Best Regards,
+Elena'''
 
 smtp_port = 465
 print('1...')
@@ -40,48 +116,40 @@ server.set_debuglevel(1)
 print('login...')
 server.login(from_addr, password)
 miss_sku = []
+send_account = 0
 for i in buys:
-    if i['order_id'] in refunds:
-        continue
+    # if i['order_id'] in refunds:
+    #     continue
     sku_info = skus.get(i['sku'])
     if not sku_info:
         miss_sku.append(i['sku'])
         continue
+    extra_str = ''
     market_domain = sku_info['market_domain']
     asin = sku_info['asin']
+    if 'B01N9AMYWA' == asin:
+        extra_str = alarm_str
     product_name = sku_info['product_name']
-    msg = MIMEText('''
-Dear %s,
-
-Greetings! This is Elena from O'Hill customer service team.
-
-This is a follow-up email to confirm whether you are completely happy with this purchase. If there was a problem with our product, please feel free to get back to us and we will help to get the problem solved ASAP. And if you are satisfied with our product and service, we would greatly appreciate it if you could share your user experience with your friends/ family or online.
-
-The following is the details for your Amazon order: %s
-
-%s
-
-To share your user experience, click: http://www.amazon.%s/dp/%s
-
-This email is for you to be completely satisfied with this transaction. If this is not the case, we would appreciate it if you would give us a chance to make things right for you before leaving a feedback. Thanks a lot.
-
-Wish you a great day ahead!
-
-Best Regards,
-Elena''' % (i['buyer_name'], i['order_id'], product_name, market_domain, asin), 'plain', 'utf-8')
-    msg['From'] = u'O\'Hill Direct<%s>' % from_addr
-    msg['To'] = u'%s<%s>' % (i['buyer_name'], i['buyer_email'])
+    msg = MIMEText(follow_str % (i['buyer_name'], i['order_id'], product_name,
+                                 extra_str, market_domain, asin),
+                   'plain', 'utf-8')
+    msg['From'] = 'O\'Hill Direct<%s>' % from_addr
+    msg['To'] = '%s<%s>' % (i['buyer_name'], i['buyer_email'])
     msg['Subject'] = Header('This is a follow-up letter about your Amazon'
                             ' Order #%s' % i['order_id'])
 
     # server = smtplib.SMTP(smtp_server, 25)
     print('sending to ...%s' % i['buyer_email'])
     server.sendmail(from_addr, [i['buyer_email']], msg.as_string())
+    # server.sendmail(from_addr, to_addr, msg.as_string())
     print('sent to %s' % i['buyer_email'])
+    send_account += 1
+
+total_buyers = len(buys)
+print('total buyers:%d, sent emails: %d' % (total_buyers, send_account))
 
 if miss_sku:
     print('Miss sku:')
     for i in miss_sku:
         print(i)
 server.quit()
-
