27f66710b73fc15249769dc7bcf802b138c3a5b7
yanheven <yanheven@qq.com>
2017-03-05 22:13:43 +0800

    upc update

--- generate_upc.py
+++ generate_upc.py
@@ -1,8 +1,11 @@
 from functools import reduce
 
-start = '693375992'
-for i in range(1000):
+start = '69415089'
+#start = raw_input('input factory code:\n')
+for i in range(10000):
     midle = ''
+    if i < 1000:
+        midle += '0'
     if i < 100:
         midle += '0'
     if i < 10:
@@ -10,10 +13,10 @@ for i in range(1000):
     midle += str(i)
     c1 = reduce(lambda x, y: x+y,
                [int(start[j]) for j in range(len(start)) if j%2==0])
-    c1 += int(midle[1])
+    c1 += int(midle[0]) + int(midle[2])
     c2 = reduce(lambda x, y: x+y,
                [int(start[j]) for j in range(len(start)) if j%2==1])
-    c2 += int(midle[0]) + int(midle[2])
+    c2 += int(midle[1]) + int(midle[3])
     c = 10 - (c1 + 3*c2) % 10
     if c == 10:
         c = 0
